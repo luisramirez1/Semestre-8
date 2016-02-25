0c272c56adf92003d6329beee32f36ff4aecf4c1
Luis Ramirez <luisramirez.lfrs@gmail.com>
2016-02-25 12:47:18 -0700

    Update Ejercicio7.pl

--- Prolog/Ejercicio7.pl
+++ Prolog/Ejercicio7.pl
@@ -13,3 +13,5 @@ elemento_par(X,List), posicion(X,List,Pos), divide(Pos,2,C,R), R=:=0.
 posicion(X,[X|_],1).
 posicion(X[_|R],Pos) :- posicion(X,R,Pos1), Pos is Pos1 + 1.
 
+pEscalar([],[],0).
+pEscalar([C|R],[C1|R1],P) :- pEscalar(R,R1,P1), P is C*C1+P1.
